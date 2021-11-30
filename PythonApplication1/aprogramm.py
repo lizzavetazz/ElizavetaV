34267db17ef77695c4e8345e4843df5eaf70fc47
opilane <opilane@HP-CZC6297B8C>
2021-11-30 14:06:36 +0200

    .

--- PythonApplication1/aprogramm.py
+++ PythonApplication1/aprogramm.py
@@ -1,4 +1,40 @@
 from module1 import*
+from keyboard import*
+palgad=loe_failist_listisse("palgad.txt")
+inimesed=loe_failist_listisse("inimesed.txt")
+
+while 1:
+    print("*"*100, "\nKeskmine - A\nMinimum - B\Maksimum - C\nOtsing - G\nLisa - V")
+    print("Vajuta nuppu==>")
+
+    if read_key()=="A":
+        kesk_palk=round(keskmine(palk),2)
+        print("Keskmine palk on ", kesk_palk)
+    elif read_key()=="B":
+        min_palk, kellel=minimum(palk,inimesed)
+        print("Minimaalne palk=> ", min_palk, "Kellel=> ",  kellel)
+    elif read_key()=="C":
+        max_palk, kellel=maksimum(palk, inimesed)
+        print("Maksimaalne palk=> ", max_palk, "Kellel=> ",  kellel)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 palgad=[1200,2500,750,395,1200]
 inimesed=["A","B","C","D","E"]
 otsing_nimi_jargi(inimesed,palgad)
--- PythonApplication1/module1.py
+++ PythonApplication1/module1.py
@@ -1,3 +1,25 @@
+def loe_failist_listisse(file:str)->list:
+    """Loeme tekst failist ja salvesta järjendisse
+    """
+    file=open(file,"r")
+    list_=[]
+    for stroka´in file:
+        list_.append(stroka.strip())
+    file.close()
+    return  list_
+
+
+#1
+def lisa(palk,inimesed)
+    """Lisame veel inimesi ja nende palgad
+    """
+    a=input("Sisesta nimi=>")
+    inimesed.append(a)
+    b=int(input("Sisesta palk=>"))
+    palk.append(b)
+    return palk,inimesed
+
+
 #2 
 def kustutamine(i,p): #удаляем человека из списка
     nimi=input("Sisestage nime, keda kustutame: ")
@@ -18,7 +40,7 @@ def kustutamine(i,p): #удаляем человека из списка
 #7 
 def otsing_nimi_jargi(inimesed:list,palgad:list):
     """Tagastame järjed või tekst
-    :rtype var:tulemus
+   :rtype var:tulemus
     """
     nimi=input("Keda otsime?")
     for inimene in inimesed:
@@ -30,10 +52,7 @@ def otsing_nimi_jargi(inimesed:list,palgad:list):
                 k=inimesed.index(nimi,b)
                 b+=k+1
                 t.append(nimi+str(palk))
-            return
+                return
         else:
             t="Ei ole nimekirjas"
             print(t)
-
-palgad=[1200,2500,750,395,1200]
-inimesed=["A","B","C","D","E"]
