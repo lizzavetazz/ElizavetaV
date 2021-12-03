7650174224be1760f504c02815a97faeb186baf2
opilane <opilane@HP-CZC6297B8C>
2021-12-03 13:25:50 +0200

    Программа

--- PythonApplication1/aprogramm.py
+++ PythonApplication1/aprogramm.py
@@ -1,40 +1,25 @@
 from module1 import*
 from keyboard import*
-palgad=loe_failist_listisse("palgad.txt")
-inimesed=loe_failist_listisse("inimesed.txt")
-
-while 1:
-    print("*"*100, "\nKeskmine - A\nMinimum - B\Maksimum - C\nOtsing - G\nLisa - V")
-    print("Vajuta nuppu==>")
-
-    if read_key()=="A":
-        kesk_palk=round(keskmine(palk),2)
-        print("Keskmine palk on ", kesk_palk)
-    elif read_key()=="B":
-        min_palk, kellel=minimum(palk,inimesed)
-        print("Minimaalne palk=> ", min_palk, "Kellel=> ",  kellel)
-    elif read_key()=="C":
-        max_palk, kellel=maksimum(palk, inimesed)
-        print("Maksimaalne palk=> ", max_palk, "Kellel=> ",  kellel)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 palgad=[1200,2500,750,395,1200]
 inimesed=["A","B","C","D","E"]
-otsing_nimi_jargi(inimesed,palgad)
+
+while True:
+    print("*"*100, "\nOtsing - 1\nMinimum - 2\Suurim - 3\nOtsing - 4\nLisa - 5")
+    print("Vajuta nuppu==>")
+    if read_key()=="1":
+        otsing_nimi_jargi=(inimesed)
+        print("Otsime=> ", otsing_nimi_jargi)
+    elif read_key()=="2":
+        min_palk, kellel=minimum(palk,inimesed)
+        print("Minimaalne palk=> ", min_palk, "Kellel=> ",  kellel)
+    elif read_key()=="3":
+        suurim_palk, kellel=maksimum(palk, inimesed)
+        print("Suurim palk=> ", suurim_palk, "Kellel=> ",  kellel)
+    elif read_key()=="4":
+        kustutamine(palk, inimesed)
+        print("Kustutamine=> ", kustutamine, "Kellel=> ",  kellel)
+    elif read_key()=="5":
+        lisa(palk,inimesed)
+        print("Lisame=> ", lisa, "Kellel=> ",  kellel)
+
+
--- PythonApplication1/module1.py
+++ PythonApplication1/module1.py
@@ -1,80 +1,100 @@
-def loe_failist_listisse(file:str)->list:
-    """Loeme tekst failist ja salvesta järjendisse
-    """
-    file=open(file,"r")
-    list_=[]
-    for stroka´in file:
-        list_.append(stroka.strip())
-    file.close()
-    return  list_
-
-
 #1
-    def lisa(palk,inimesed):
-        """Lisame veel inimesi ja nende palgad
-        """
-        a=input("Sisesta nimi=>")
-        inimesed.append(a)
-        b=int(input("Sisesta palk=>"))
-        palk.append(b)
-        return palk,inimesed
+def otsing_nimi_jargi(inimesed, palgad):
+    """Tagastame järjed või tekst
+    :param list inimesed: Inimeste nimekiri
+    :param list palgad: Palgade kogum
+    :rtype: list, list 
+    """
+    nimi=input("Keda otsime?")
+    for inimene in inimesed:
+        if inimene==nimi:
+            n=inimesed.count(nimi)
+            print("Inimene on olemas, selline nimi kohtume ",n,"korda")
+            b=0
+            for i in range(n):
+                k=inimesed.index(nimi,b)
+                b+=k+1
+                t.append(nimi+str(palgad))
+                return palgad,inimesed
+        else:
+            t="Ei ole nimekirjas"
+     
+
 
 
 #2 
-    def kustutamine(inimesed,palgad): #удаляем человека из списка
-        """Kustutame inimesi ja palga
-        """
-        nimi=input("Sisestage nime, keda kustutame: ")
-        n=i.count(nimi)
-        abi_list=[]
-        if n>0:
-            t=0
-            for e in range(len(i)):
-                if i[e]==nimi:
-                    t+=1
-                    abi_list.append(int(e)) #список индексов
-                    print(t,". ",i[e],"-", p[e])
-                    jaar=int(input("Järjenumber: "))
-                    i.pop(abi_list[jaar-1])
-                    p.pop(abi_list[jaar-1])
-                    andmed_ekranile(i,p)
-
-#7 
-    def otsing_nimi_jargi(inimesed:list,palgad:list):
-        """Tagastame järjed või tekst
-        :rtype var:tulemus
-        """
-        nimi=input("Keda otsime?")
-        for inimene in inimesed:
-            if inimene==nimi:
-                n=inimesed.count(nimi)
-                print("Inimene on olemas, selline nimi kohtume ",n,"korda")
-                b=0
-                for i in range(n):
-                    k=inimesed.index(nimi,b)
-                    b+=k+1
-                    t.append(nimi+str(palk))
-                    return palk,inimesed
-            else:
-                t="Ei ole nimekirjas"
-                print(t)
+def min_palk():
+    """Programm kontrollib ja nätab välja ekraanile inimesi, kelell on kõige väiksem palk
+    :rtype str nimi: inimese nimi, kellel on kõige väiksem palk
+    """
+    palgad=[]
+    with open("palgad.txt", "r") as f1:
+        for strok in f1:
+            palgad.append(strok.strip())
+            inimesed=[]
+            f=open("login.txt", "r")
+            inimesed=[]
+        for stroka in f:inimesed.append(stroka.strip())
+        zp=palgad.copy()
+        zp.sort()
+        zp.reverse()
+        a=zp[0]
+        b=palgad.index(a)
+        nimi=inimesed[b]
+    return nimi
 
 #3
-    def suurim_palk():
-        """Arvutama kõike suureim palga
-        """
-        palgad=[]
-        with open ("palgad.txt", "r") as f1:
-            for stroka in f1:
-                palgad.append(stroka.strip())
-                f=open("inimesed.txt", "r")
-                inimesed=[]
-                for stroka in f:
-                    inimesed.append(stroka.strip())
-                    f.close
-                    palgadS=palgad.copy()
-                    palgadS.sort()
-                    a=palgadS[0]
-                    b=palgad.index(a)
-                    print("Kõige suureim palg on "+inimesed[b]+" palga")
+def suurim_palk():
+    """Programm kontrollib ja nätab välja ekraanile inimesi, kelell on kõige suurem palk
+    :rtype str nimi: inimese nimi, kellel on kõige suurem palk
+    """
+    palgad=[]
+    with open ("palgad.txt", "r") as f1:
+        for stroka in f1:
+            palgad.append(stroka.strip())
+            f=open("inimesed.txt", "r")
+            inimesed=[] 
+        for stroka in f:
+            inimesed.append(stroka.strip())
+            f.close
+            palgadS=palgad.copy()
+            palgadS.sort()
+            a=palgadS[0]
+            b=palgad.index(a)
+            inimesed[b]
+        return nimi
+#4
+def kustutamine(inimesed,palgad): #удаляем человека из списка
+    """Kustutame inimesi ja palga palgast
+    :param list inimesed: Inimeste nimekiri
+    :param list palgad: Palgade kogum
+    :rtype: list, list 
+    """
+    nimi=input("Sisestage nime, keda kustutame: ")
+    n=i.count(nimi)
+    abi_list=[]
+    if n>0:
+        t=0
+        for e in range(len(i)):
+            if i[e]==nimi:
+                t+=1
+                abi_list.append(int(e)) #список индексов
+                print(t,". ",i[e],"-", p[e])
+                jaar=int(input("Järjenumber: "))
+                i.pop(abi_list[jaar-1])
+                p.pop(abi_list[jaar-1])
+                andmed_ekranile(i,p)
+            return inimesed, palgad
+
 #5
+def lisa(inimesed, palgad):
+    """Lisame veel inimesi ja nende palgad listisse
+    :param list inimesed: Inimeste nimekiri
+    :param list palgad: Palgade kogum
+    :rtype: list, list 
+    """
+    a=input("Sisesta nimi=>")
+    inimesed.append(a)
+    b=int(input("Sisesta palk=>"))
+    palgad.append(b)
+    return palgad,inimesed
